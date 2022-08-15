01751bcbe79a23cc4d2ad352298c14fbb470daae
Jorden-Brown <jordenbrown17@gmail.com>
2022-08-15 13:46:00 -0400

    Hellow World!

---
+++ src/main/java/com/jordenbrown/basicbank/JAB_BANK/JabBankApplication.java
@@ -0,0 +1,13 @@
+package com.jordenbrown.basicbank.JAB_BANK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JabBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JabBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jordenbrown/basicbank/JAB_BANK/JabBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordenbrown.basicbank.JAB_BANK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JabBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
